177870ad410fbee4a3337148d18e3f4425f24d63
Filippo Tortomasi <filippo.tortomasi@gmail.com>
2017-12-11 16:58:27 +0100

    Added monitor customizations to ddcctl.sh

--- ddcctl.sh
+++ ddcctl.sh
@@ -1,34 +1,55 @@
 #!/bin/bash
 #tweak OSX display monitors' brightness to a given scheme, increment, or based on the current local time
 
-hp="ddcctl -d 1"
-len="ddcctl -d 2"
+# I: got edid.name: LG Ultra HD -> lg4k
+# I: got edid.name: E2442 -> lg1080
+# lg4k sometimes reports 0 when reading brightness
+# lg1080 always reports 0 brightness
+#
+# result=($(ddcctl -d $lg4k -b $1 -c $2 | awk '/current:/ {print $8}'))
+# brightness=${result[0]/,/}
+# contrast=${result[1]/,/}
+# ddcctl -d $lg1080 -b $brightness -c $contrast
+	
+lg4k="/usr/local/bin/ddcctl -d 1"
+lg1080="/usr/local/bin/ddcctl -d 2"
 
-dim() {
-	$hp -b 42 -c 26
-	$len -b 4 -c 9
-}
+setDisplays() {
+	if [[ $# -lt 1 ]] ; then
+    	echo "setDisplay [down|up]"
+		exit 1
+	fi
 
-bright() {
-	$hp -b 100 -c 75
-	$len -b 85 -c 80
-}
+	if [[ ! -f "current.txt" ]]; then
+		echo '70' > current.txt
+	fi
+	
+	current=$(cat current.txt)
 
-up() {
-	$hp -b 20+ -c 12+
-	$len -b 15+ -c 12+
-}
+	if [[ "$1" == "up" ]] ; then
+		newBrightness=$((current+10))
+	else 
+		newBrightness=$((current-10))
+	fi
 
-down() {
-	$hp -b 20- -c 12-
-	$len -b 15- -c 12-
+	newBrightness=$(($newBrightness>100?100:$newBrightness))
+	newBrightness=$(($newBrightness<0?0:$newBrightness))	
+	newContrast=$(echo "$newBrightness * $newBrightness * -0.003 + $newBrightness * 0.85" | bc)
+
+	$lg4k -b $newBrightness -c $newContrast > /dev/null &
+	$lg1080 -b $newBrightness -c $(echo "$newContrast * 0.636363" | bc) > /dev/null &
+
+	echo $newBrightness > current.txt
+	echo $newBrightness $newContrast
 }
 
 case "$1" in
-	dim|bright|up|down) $1;;
-	*)	#no scheme given, match local Hour of Day
-		HoD=$(date +%k) #hour of day
-		let "night = (( $HoD < 7 || $HoD > 18 ))" #daytime is 7a-7p
-		(($night)) && dim || bright
-		;;
+	-b) setDisplays $2;;
+	# *)	#no scheme given, match local Hour of Day
+	# 	# HoD=$(date +%k) #hour of day
+	# 	# let "night = (( $HoD < 7 || $HoD > 18 ))" #daytime is 7a-7p
+	# 	# (($night)) && dim || bright
+	# 	# ;;
+	# 	setDisplays up
+	# 	;;
 esac
